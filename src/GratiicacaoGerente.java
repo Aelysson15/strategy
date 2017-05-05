95d904b407c7c7b7814e73f1de5413125d757b47
Aelysson15 <j.aelysson@gmail.com>
2017-05-04 21:39:56 -0300

    Update GratiicacaoGerente.java

--- src/GratiicacaoGerente.java
+++ src/GratiicacaoGerente.java
@@ -1,7 +1,7 @@
 
 public class GratiicacaoGerente implements Gratificacao{
 	@Override
-    public double calcularGratificacao(Funcionario umGerente) {
+    public double calcularGratificacao(Funcionario umFuncionario) {
           if (umFuncionario.getNivel() >= 2) {
               return umFuncionario.getSalarioBase() * 1.45;
           }
