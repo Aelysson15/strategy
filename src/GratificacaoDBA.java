6ba110de2ad89c079216db4abf9e0d25b1403c58
Aelysson15 <j.aelysson@gmail.com>
2017-05-04 21:37:40 -0300

    Update GratificacaoDBA.java

--- src/GratificacaoDBA.java
+++ src/GratificacaoDBA.java
@@ -1,7 +1,7 @@
 
 public class GratificacaoDBA implements Gratificacao{
 	@Override
-    public double calcularGratificacao(Funcionario umFuncionario) {
+    public double calcularGratificacao(Funcionario umDBA) {
           if (umFuncionario.getNivel() >= 2) {
               return umFuncionario.getSalarioBase() * 1.30;
           }
