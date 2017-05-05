637c40731e93b34e2ba5c42ef4d818df15565f63
Aelysson15 <j.aelysson@gmail.com>
2017-05-04 21:35:49 -0300

    Update Gratificacao.java

--- src/Gratificacao.java
+++ src/Gratificacao.java
@@ -2,4 +2,6 @@
 
 interface Gratificacao {
 	double calcularGratificacao(Funcionario umFuncionario);
+	double calcularGratificacao(Funcionario umDBA);
+	double calcularGratificacao(Funcionario umGerente);
 }
